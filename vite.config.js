85d08c7c8d4e6a4471d1d16db5ff2db04ae02883
gilbert-ku <gilbert45ku@gmail.com>
2023-10-01 02:48:26 +0300

    add dist for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,17 @@
-const path = require('path')
+import path from 'path';
 
 export default {
   root: path.resolve(__dirname, 'src'),
   resolve: {
     alias: {
       '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
-    }
+    },
   },
   server: {
     port: 8080,
-    hot: true
-  }
-}
+    hot: true,
+  },
+  build: {
+    outDir: 'dist', // Specify the output directory for the production build
+  },
+};
